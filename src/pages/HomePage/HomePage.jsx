411237084ab2f649cf7885f6765933549065ef6d
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-06-03 20:16:54 +0300

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,11 @@
+import styles from './HomePage.module.css';
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.titleWrapper}>
+      <h1 className={styles.titleHomePage}>
+        A tweet card application with interactive features for the user.
+      </h1>
+    </div>
+  );
 };
 export default HomePage;
