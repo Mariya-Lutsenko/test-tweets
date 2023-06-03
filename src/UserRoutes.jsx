84c30aab211dc35d3cc8c064bfb020b06ffb2773
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-06-03 20:01:20 +0300

    Formated Pretier

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,15 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "pages/HomePage/HomePage";
-import TweetsPage from "pages/TweetsPage/TweetsPage";
-
+import { Route, Routes } from 'react-router-dom';
+import HomePage from 'pages/HomePage/HomePage';
+import TweetsPage from 'pages/TweetsPage/TweetsPage';
 
 const UserRoutes = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/tweets" element={<TweetsPage/>}/>
-            <Route path="*" element={<HomePage/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tweets" element={<TweetsPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  );
+};
 export default UserRoutes;
